9aba3cb4eb31297721baa2901c0833a9588b1707
ibrahim mohammed <120165878+ibrahimmaliraqi@users.noreply.github.com>
2026-09-06 00:39:32 +0300

    Add database connection check to root route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.send('Server is running successfully 🚀');
+var dbHelper = require('../helpers/db_helpers');
+
+router.get('/', function (req, res) {
+
+    dbHelper.query(
+        'SELECT 1 AS connected',
+        [],
+        function (error, result) {
+
+            if (error) {
+                return res.status(500).json({
+                    success: false,
+                    server: 'running',
+                    database: 'disconnected',
+                    message: 'Database connection failed',
+                    error: error.message
+                });
+            }
+
+            res.status(200).json({
+                success: true,
+                server: 'running',
+                database: 'connected',
+                message: 'Server and database are working successfully',
+                result: result
+            });
+        }
+    );
+
 });
 
 module.exports = router;
